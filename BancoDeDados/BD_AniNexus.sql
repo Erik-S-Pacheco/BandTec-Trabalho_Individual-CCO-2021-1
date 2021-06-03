0b29a288a01ec1c864a2aa143ec844d5ad65145b
Your Name <erik.pacheco@bandtec.com.br>
2021-06-03 16:44:18 -0300

    Criação da página chat para que possa ser inserido a api do JP, mais alguns ajustes nos designs de algumas páginas e reestruturação da Modelagem e do Script do banco de dados

--- BancoDeDados/BD_AniNexus.sql
+++ BancoDeDados/BD_AniNexus.sql
@@ -5,9 +5,27 @@ create table tbNoticia (
 	idNoticia int primary key auto_increment,
     dataNotica varchar(45),
     tituloNoticia varchar(50),
-    textoNoticia varchar(255)
+    textoNoticia varchar(1000)
+);
+ 
+create table tbGuiaTemporada (
+	idTemporada int primary key auto_increment,
+    thumbnailTemporada varchar(45),
+    tituloTemporada varchar(45),
+    anoTemporada char(4),
+    mesTemporada varchar(9),
+    dataPublicacao varchar(45)
 );
 
+create table tbAnime (
+	idAnime int primary key auto_increment,
+    nomeAnime varchar(50),
+    fonte varchar(30),
+    genero varchar(45),
+    diretor varchar(45),
+    sinopse varchar(500)
+);
+ 
 
 create table Usuario (
 	idUsuario int primary key auto_increment,
@@ -16,16 +34,25 @@ create table Usuario (
     senha varchar(50)
 );
 
+insert into Usuario (nomeUsuario, email, senha) values
+('admin', 'admin@gmail.com', 'admin123');
+
 select * from Usuario;
 
-create table tbComentario (
- fkNoticiaComentario int,
- foreign key (fkNoticiaComentario) references tbNoticia(idNoticia),
- fkUsuarioComentario int,
- foreign key (fkUsuarioComentario) references Usuario(idUsuario),
- dataComentario date,
- primary key (fkNoticiaComentario, fkUsuarioComentario, dataComentario),
- textoComentario varchar(255)
+create table tbChat (
+	idDescricao int primary key auto_increment,
+    descriacao varchar(150),
+    fkUsuarioDescricao int,
+    foreign key (fkUsuarioDescricao) references Usuario (idUsuario)
+);
+
+
+create table tbAvaliacaoAnime (
+	fkUsuarioAvaliacao int,
+    foreign key (fkUsuarioAvaliacao) references Usuario (idUsuario),
+    fkAnimeAvaliacao int,
+    foreign key (fkAnimeAvaliacao) references tbAnime (idAnime),
+    notaAvaliacao decimal(4,2)
 );
 
 
